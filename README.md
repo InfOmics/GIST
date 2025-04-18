78e6df4ee721cad75ed3457dc61e6b2f31701649
gospelnnadi <nnadigospel@gmail.com>
2025-04-18 16:35:09 +0200

    cleaned up code

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
 # GIST
 Leveraging Graph Information for Spatially Informed Patient Data Analysis with GIST
 
+Create an environment if necessary
 ````
 conda create -n gist python==3.10.0 r-base==4.3.1 -y
-
+conda activate gist
 ````
+Verify R home is in the conda environment 
 ````
 which R
 
 ````
 /home/youruser/anaconda3/envs/gist/lib/R
 
-
+install requirements
 ````
 pip install -r requirement.txt
 
 ````
+Install mclust packages
 ````
 Rscript -e 'install.packages("mclust",repos="http://cran.us.r-project.org")'
 
 ````
-
+install GIST packages
 ````
 pip install git+https://github.com/gospelnnadi/GIST.git
 
 ````
 
+run_GIST.py  contains the GIST pipeline. 
--- run_GIST.py
+++ run_GIST.py
@@ -1,11 +1,115 @@
   
-import argparse
 from GIST.GIST import GIST
 import torch
-from prepare_ground_truth import *
 from GIST.utils.clustering import clusters_n_plot
 device =  "cuda" if torch.cuda.is_available() else "cpu"
 
+import scanpy as sc
+import numpy as np
+import pandas as pd
+
+
+def read_adata(path, is_h5ad=False):
+    #Todo: add check if Visium, if 'filtered_feature_bc_matrix.h5' or 'data_name_filtered_feature_bc_matrix.h5'
+    if is_h5ad:
+        adata = sc.read_h5ad(path)
+        adata.var_names_make_unique()
+        adata.obsm["spatial"]=adata.obsm["spatial"].astype(float)
+    else: 
+        adata = sc.read_visium(path, count_file='filtered_feature_bc_matrix.h5', load_images=True)
+        adata.var_names_make_unique()
+        adata.obsm["spatial"]=adata.obsm["spatial"].astype(float)
+    return adata
+
+
+
+
+
+def fromlayerstonumber (layers):
+  res=[]
+  for sub in layers:
+    if sub == 'Layer1':
+      res.append(str(sub).replace('Layer1', '1'))
+    elif sub == 'Layer2':
+      res.append(str(sub).replace('Layer2', '2'))
+    elif sub == 'Layer3':
+      res.append(str(sub).replace('Layer3', '3'))
+    elif sub == 'Layer4':
+      res.append(str(sub).replace('Layer4', '4'))
+    elif sub == 'Layer5':
+      res.append(str(sub).replace('Layer5', '5'))
+    elif sub == 'Layer6':
+      res.append(str(sub).replace('Layer6', '6'))
+    elif sub == 'WM':
+      res.append(str(sub).replace('WM', '7'))
+    elif str(sub)=='nan' :
+      res.append( res[-1]) ##nan
+  return res
+
+def fromlayerstonumberMBA (df_meta_layer):
+    label=1
+    for i in np.unique(df_meta_layer):
+        df_meta_layer[np.where(df_meta_layer == i )[0]]=label
+        label+=1
+    return df_meta_layer
+
+def fromlayerstonumberMHC(adata):
+    adata.obs["ground_truth"] = (
+    adata.obs["cluster"].astype("category").cat.codes + 1
+).astype(str)
+
+   
+def fromlayerstonumberMVC(adata):
+   adata.obs["ground_truth"] = (
+    adata.obs["label"].astype("category").cat.codes + 1
+).astype(str)
+
+
+
+
+
+
+def get_adata(path='',data_name='',  is_h5ad=False):
+
+
+    if path=='':
+        adata =read_adata('inputs/Data/DLPFC/151673' )
+        annotation_path="inputs/Data/DLPFC/151673/metadata.tsv"
+        df_meta = pd.read_csv(annotation_path, sep='\t')
+        df_meta_layer = df_meta['layer_guess']
+        adata.obs['ground_truth'] = fromlayerstonumber (df_meta_layer.values)  
+    else:
+        adata =read_adata(path, is_h5ad)
+        print("data name:", data_name)
+
+    if "Human_Breast_Cancer" in data_name :
+        df_meta = pd.read_csv(f"{path}/metadata.tsv", sep='\t')
+        df_meta_layer = df_meta['fine_annot']
+        adata.obs['ground_truth'] =df_meta_layer.values 
+        print(f"Data {data_name} contains annotation")
+    elif "Mouse_Brain_Anterior" in data_name:
+        df_meta = pd.read_csv(f"{path}/metadata.tsv", sep='\t')
+        df_meta_layer = df_meta['ground_truth']       
+        adata.obs['ground_truth'] = np.array(fromlayerstonumberMBA (df_meta_layer)).astype(str) 
+        print(f"Data {data_name} contains annotation")
+    elif "DLPFC" in data_name: 
+        annotation_path= f"{path}/metadata.tsv"
+        df_meta = pd.read_csv(annotation_path, sep='\t')
+        df_meta_layer = df_meta['layer_guess']
+        adata.obs['ground_truth'] = fromlayerstonumber (df_meta_layer.values)  
+        print(f"Data {data_name} contains annotation")
+    elif "Mouse_Hippocampus" in data_name: 
+        fromlayerstonumberMHC (adata)  
+        print(f"Data {data_name} contains annotation")
+    elif "Mouse_Visual_Cortex" in data_name: 
+        fromlayerstonumberMVC (adata)  
+        print(f"Data {data_name} contains annotation")
+    elif 'ground_truth' in adata.obs and len(adata.obs['ground_truth']):
+         print(f"Data {data_name} contains annotation")
+    else: 
+        print(f"Data {data_name} does not have annotation")
+
+    return adata
 
 
 def get_cluster_size(data_name):
@@ -42,6 +146,8 @@ def get_cluster_size(data_name):
          plot_size=35
     return n_cluster, plot_size
 
+
+
 adata=get_adata()
 seed=35
 GISTModel=GIST(adata=adata, device=device, random_seed=seed, is_visium='Visium')
@@ -51,35 +157,4 @@ adata.write_h5ad(f"inputs/Preprocessed/{data_name}.h5ad")
 n_cluster, plot_size=get_cluster_size(data_name)
 clusters_n_plot(adata, adata.obsm["DGSI"], f"outputs/{data_name}.png", n_cluster, refinement=True,plot_size=plot_size,seed=seed, is_visium=GISTModel.is_visium)
 
-""" from mclustpy import mclustpy
-from sklearn.decomposition import PCA
-def mClust (X , n_clusters=7):     
-    pca = PCA(n_components=20, random_state=42) 
-    data = pca.fit_transform(X)
-    res = mclustpy(data, G=n_clusters, modelNames='EEE', random_seed=222)
-    mclust_res =np.array(res['classification']).astype(int)
-    print(np.unique(mclust_res))
-    return mclust_res
 
-import scanpy as sc
-adata = sc.read_h5ad(f"inputs/Preprocessed/DLPFC_151673.h5ad")
-data_name='DLPFC_151673'
-n_cluster, plot_size=get_cluster_size(data_name)
-#mClust (adata.obsm["DGSI"] , n_clusters=7)
-clusters_n_plot(adata, adata.obsm["DGSI"], f"outputs/{data_name}.png", n_cluster, refinement=True,plot_size=plot_size,  is_visium=True) """
-
-
-    
-
-
-""" if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=" Create the graph for BertWalkST.", formatter_class=argparse.ArgumentDefaultsHelpFormatter,)
-    parser.add_argument("--data_name", type=str, default="DLPFC_151673", help="Data name to distiguish graphs of the different dataset.")
-    parser.add_argument("--is_visium", type=int, default=1, help="Data technology is visium or not. Default is visium 1")
-    parser.add_argument("--is_h5ad", type=int, default=0, help="Data is scanpy h5ad or not. Default is not 0")
-    parser.add_argument("--data_path", type=str, default="inputs/spatial_data/Data/1.DLPFC/151673", help="full path to the dataset.")
-    #parser.add_argument("--use_svg", type=int, default=0, help="Specify if to use Spatially variable genes or not")
-    parser.add_argument("--seed", type=int, default=35, help="Random seed")
-    parser.add_argument("--plot_size", type=int, default=0, help="Tissue spatial plot size")
-    args= parser.parse_args()
-    create_graph(args.data_path, args.data_name, args.is_visium,args.is_h5ad, args.seed , args.plot_size) """
