2724d881d0998456840205c85e05c78e9a99363f
gospelnnadi <nnadigospel@gmail.com>
2026-02-23 19:13:40 +0100

    rename init import

--- GIST/utils/__init__.py
+++ GIST/utils/__init__.py
@@ -4,6 +4,7 @@
 # File Name: __init__.py
 # Description:
 """
-from .clustering import mclust_clustering, plot_n_evaluate_cluster
+from .clustering import clustering_method, plot_cluster, evaluate_cluster
 from .silhouette_spatial import silhouette_spatial_score  
 from .utilities import pca, set_seed
+
